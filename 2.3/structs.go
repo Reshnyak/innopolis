d8912e3a7919634ea2406989276177d121cfc7d0
Denis Reshnyak <reshnyakdg@gmail.com>
2024-05-26 19:44:33 +0300

    add 2.3 task

---
+++ 2.3/structs.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Grade int    `json:"grade"`
+}
+
+type Object struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+type Result struct {
+	ObjectID  int `json:"object_id"`
+	StudentID int `json:"student_id"`
+	Result    int `json:"result"`
+}
+type ContolSection struct {
+	Students []Student
+	Objects  []Object
+	Results  []Result
+}
+
+// Метод возвращающий имя студента по его id
+func (cs ContolSection) GetStudentNameByID(id int) string {
+	for _, s := range cs.Students {
+		if s.ID == id {
+			return s.Name
+		}
+	}
+	return ""
+}
+
+// Метод возвращающий grade студента по его id
+func (cs ContolSection) GetStudentGradeByID(id int) int {
+	for _, s := range cs.Students {
+		if s.ID == id {
+			return s.Grade
+		}
+	}
+	return 0
+}
+
+// Метод возвращающий наименование предмета по id
+func (cs ContolSection) GetObjectNameByID(id int) string {
+	for _, obj := range cs.Objects {
+		if obj.ID == id {
+			return obj.Name
+		}
+	}
+	return ""
+}
+
+//Форматированный вывод =) ...надо попробовать повторить c template
+func (cs ContolSection) PrintControlSection() {
+	fmt.Println("___________________________________________________")
+	fmt.Printf("Student name \t| Grade\t| Object\t| Resulte |\n")
+	fmt.Println("___________________________________________________")
+	for _, r := range cs.Results {
+		fmt.Printf("%s \t\t| ", cs.GetStudentNameByID(r.StudentID))
+		fmt.Printf(" %d\t| ", cs.GetStudentGradeByID(r.StudentID))
+		objName := cs.GetObjectNameByID(r.ObjectID)
+		if len(objName) < 5 {
+			fmt.Printf(" %s\t\t| ", cs.GetObjectNameByID(r.ObjectID))
+		} else {
+			fmt.Printf(" %s\t| ", cs.GetObjectNameByID(r.ObjectID))
+		}
+		fmt.Printf(" %d\t  |\n", r.Result)
+	}
+
+}
