71692b9eb14ef38627e0f5e14da93051db75d26b
Denis Reshnyak <reshnyakdg@gmail.com>
2024-06-05 00:21:56 +0300

    fix 2.1

--- 2.1/main.go
+++ 2.1/main.go
@@ -8,15 +8,20 @@ import (
 // находит пересечение неопределенного количества слайсов типа int
 func sliceIntersection(slices ...[]int) []int {
 	var res []int
-
+	if len(slices) == 0 {
+		return res
+	}
 	// Создаем мапу для подсчета общего количества уникальных чисел
 	countMap := make(map[int]int)
 	for _, slice := range slices {
+		if len(slice) == 0 {
+			return []int{}
+		}
 		// Создаем мапу уникальных чисел в слайсе
-		uniqueMap := make(map[int]bool)
+		uniqueMap := make(map[int]struct{})
 		for _, num := range slice {
-			if !uniqueMap[num] {
-				uniqueMap[num] = true
+			if _, ok := uniqueMap[num]; !ok {
+				uniqueMap[num] = struct{}{}
 			}
 		}
 		//Добавляем в общую мапу
@@ -40,5 +45,5 @@ func sliceIntersection(slices ...[]int) []int {
 func main() {
 	fmt.Println(sliceIntersection([]int{1, 2}, []int{3, 2}))
 	fmt.Println(sliceIntersection([]int{1, 2, 3, 2}, []int{3, 2}))
-	fmt.Println(sliceIntersection([]int{1, 2, 3, 2}, []int{3, 2}, []int{}))
+	fmt.Println(sliceIntersection([]int{}, []int{1, 2, 3, 2}, []int{3, 2}))
 }
